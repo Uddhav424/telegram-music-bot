6599d7925f60574e7c5a342aa0aa34d42f5d154a
Uddhav424 <uddhavthakur2411@gmail.com>
2025-07-02 20:51:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
+API_ID = int(os.environ.get("28864821"))
+API_HASH = os.environ.get("a3a7c48e39c5fca814e207829b3159dd")
 BOT_TOKEN = os.environ.get("8186601797:AAEP_-fHZO8yCO8tIUqYcjmHWKJeVEHDAtY")
