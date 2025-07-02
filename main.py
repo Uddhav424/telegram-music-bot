c0ee9f7bb33e1ca161d48f7de208fe6d28a52f78
Uddhav424 <uddhavthakur2411@gmail.com>
2025-07-02 20:50:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from pyrogram import Client
 from pytgcalls import PyTgCalls
-from config import API_ID, API_HASH, 8186601797:AAEP_-fHZO8yCO8tIUqYcjmHWKJeVEHDAtY
+from config import 28864821, a3a7c48e39c5fca814e207829b3159dd, 8186601797:AAEP_-fHZO8yCO8tIUqYcjmHWKJeVEHDAtY
 from helper import start_handler
 
-app = Client("music_bot", api_id=API_ID, api_hash=API_HASH, bot_token=8186601797:AAEP_-fHZO8yCO8tIUqYcjmHWKJeVEHDAtY)
+app = Client("music_bot", api_id=28864821, api_hash=a3a7c48e39c5fca814e207829b3159dd, bot_token=8186601797:AAEP_-fHZO8yCO8tIUqYcjmHWKJeVEHDAtY)
 pytgcalls = PyTgCalls(app)
 
 @app.on_message()
