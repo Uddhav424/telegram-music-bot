9a114d54ee6c1afdc1e0a085a97a959a4014f084
Uddhav424 <uddhavthakur2411@gmail.com>
2025-07-03 10:17:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pyrogram import Client
 
 api_id = 28864821  # Replace with your actual API ID
 api_hash = "a3a7c48e39c5fca814e207829b3159dd"  # Replace with your actual API hash
-bot_token = "8188601797:AAEP-_fHZ0ByC08tIUqVcjmHWkJeVEHDAtY"  # Replace with your actual bot token
+bot_token = "8186601797:AAEP-_AAG6RZYqpB62-L5b9mygzy8subfLFl7XR8o"  # Replace with your actual bot token
 
 app = Client(
     "my_bot",
