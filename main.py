182f632faa02526cc8fc21029d4d901fc938f4a1
Uddhav424 <uddhavthakur2411@gmail.com>
2025-07-08 21:55:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
-from pyrogram import Client
+import os
+from pyrogram import Client, idle
+from pytgcalls import PyTgCalls
+from pytgcalls import idle as pyidle
+from config import bot, call_py
 
-api_id = 28864821  # Replace with your actual API ID
-api_hash = "a3a7c48e39c5fca814e207829b3159dd"  # Replace with your actual API hash
-bot_token = "8186601797:AAEP-_AAG6RZYqpB62-L5b9mygzy8subfLFl7XR8o"  # Replace with your actual bot token
-
-app = Client(
-    "my_bot",
-    api_id=api_id,
-    api_hash=api_hash,
-    bot_token=bot_token
-)
-
-@app.on_message()
-def hello(client, message):
-    message.reply("Hello! Bot is running successfully ✅")
-
-if __name__ == "__main__":
-    app.run()  # ✅ 4 spaces
+bot.start()
+print("UserBot Started")
+call_py.start()
+print("Vc Client Started")
+pyidle()
+idle()
