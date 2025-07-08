1fd8112b041f4f4a5597330c3e7e6dd3b72cb543
Uddhav424 <uddhavthakur2411@gmail.com>
2025-07-08 22:06:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,31 @@
-# config.py
+import os
+from dotenv import load_dotenv
+from pyrogram import Client, filters
+from pytgcalls import PyTgCalls
 
-API_ID = 28864821
-API_HASH = "a3a7c48e39c5fca814e207829b3159dd"
-BOT_TOKEN = "8188601797:AAEP-_fHZ0ByC08tIUqVcjmHWkJeVEHDAtY"
+# For Local Deploy
+if os.path.exists(".env"):
+    load_dotenv(".env")
+    
+# Necessary Vars
+API_ID = int(os.getenv("API_ID", "6"))
+API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+SESSION = os.getenv("SESSION")
+HNDLR = os.getenv("HNDLR", "!")
+GROUP_MODE = os.getenv("GROUP_MODE", "True")
 
+
+contact_filter = filters.create(
+    lambda _, __, message:
+    (message.from_user and message.from_user.is_contact) or message.outgoing
+)
+
+
+if GROUP_MODE == ("True" or "true"):
+    grp = True
+else:
+    grp = False
+
+GRPPLAY = grp
+bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="VCBot"))
+call_py = PyTgCalls(bot)
